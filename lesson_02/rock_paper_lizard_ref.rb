7a1d479a4d579ad0e3bf362a9da19f53e36600f6
Ben Rodenhäuser <ben@karo.berlin>
2017-03-10 01:59:45 +0100

    some improvements

--- lesson_02/rock_paper_lizard_ref.rb
+++ lesson_02/rock_paper_lizard_ref.rb
@@ -1,7 +1,6 @@
 require 'yaml'
 MESSAGES = YAML.load_file('./rock_paper_lizard_config.yaml')
 
-
 VALID_INPUTS = %w(r p sc l sp)
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 WIN_PAIRS = [
@@ -40,27 +39,33 @@ def display_choices(choices)
   prompt(
     'choices_made_were',
     {
-      player_choice: choices[:player_choice],
-      comp_choice: choices[:comp_choice]
+      player_choice: choices[:player],
+      comp_choice: choices[:computer]
     }
   )
 end
 
-def evaluate_round(choices)
-  if WIN_PAIRS.include?([choices[:player_choice], choices[:comp_choice]])
-    'player_won_round'
-  elsif WIN_PAIRS.include?([choices[:comp_choice], choices[:player_choice]])
-    'computer_won_round'
-  else
-    'a_tie'
+def determine_winner(choices)
+  if WIN_PAIRS.include?([choices[:player], choices[:computer]])
+    :player
+  elsif WIN_PAIRS.include?([choices[:computer], choices[:player]])
+    :computer
   end
 end
 
-def update_scores(scores, round_result)
-  if round_result == 'player_won_round'
-    scores[:player_score] += 1
-  elsif round_result == 'computer_won_round'
-    scores[:computer_score] += 1
+def display_round_result(round_winner)
+  case round_winner
+  when :player then prompt('player_won_round')
+  when :computer then prompt('computer_won_round')
+  when nil then prompt('a_tie')
+  end
+end
+
+def update_scores(scores, round_winner)
+  if round_winner == :player
+    scores[:player] += 1
+  elsif round_winner == :computer
+    scores[:computer] += 1
   end
 end
 
@@ -68,19 +73,19 @@ def display_scores(scores)
   prompt(
     'current_scores_are',
     {
-      player_score: scores[:player_score],
-      computer_score: scores[:computer_score]
+      player_score: scores[:player],
+      computer_score: scores[:computer]
     }
   )
 end
 
 def game_winner?(scores)
-  scores[:player_score] == 5 || scores[:computer_score] == 5
+  scores[:player] == 5 || scores[:computer] == 5
 end
 
 def display_game_winner(scores)
-  prompt("computer_won_game") if scores[:computer_score] == 5
-  prompt("player_won_game") if scores[:player_score] == 5
+  prompt('computer_won_game') if scores[:computer] == 5
+  prompt('player_won_game') if scores[:player] == 5
 end
 
 def request_rematch
@@ -99,20 +104,20 @@ prompt('how_to_exit')
 
 loop do
   scores = {
-    player_score: 0,
-    computer_score: 0
+    player: 0,
+    computer: 0
   }
 
   loop do
     choices = {
-      player_choice: request_choice(),
-      comp_choice: VALID_CHOICES.sample()
+      player: request_choice(),
+      computer: VALID_CHOICES.sample()
     }
 
     display_choices(choices)
-    round_result = evaluate_round(choices)
-    prompt(round_result)
-    update_scores(scores, round_result)
+    round_winner = determine_winner(choices)
+    display_round_result(round_winner)
+    update_scores(scores, round_winner)
     display_scores(scores)
     if game_winner?(scores)
       display_game_winner(scores)
--- lesson_02/test.rb
+++
@@ -1,5 +0,0 @@
-require 'yaml'
-config = File.read('./rock_paper_lizard_config.yaml')
-MESSAGES = YAML.load(config)
-
-puts MESSAGES['bye']
