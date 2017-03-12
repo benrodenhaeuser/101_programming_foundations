07717040de9c9f800ec96726e361644c31b04f46
Ben Rodenhäuser <ben@karo.berlin>
2017-03-12 01:05:32 +0100

    complete lesson 03

---
+++ lesson_03/hard1.rb
@@ -0,0 +1,8 @@
+if false
+  greeting = “hello world”
+end
+
+p greeting
+
+# expect "undefined variable" exception -- that is false though :)
+# what is happening is that the variable is initialized, but not assigned!
