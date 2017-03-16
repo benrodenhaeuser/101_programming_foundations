402eac2b27b17f54507311a7b099d93afd4976d2
Ben Rodenhäuser <ben@karo.berlin>
2017-03-16 19:34:19 +0100

    insertion sort

--- my_exercises/insertion_sort.rb
+++ my_exercises/insertion_sort.rb
@@ -1,57 +1,30 @@
 # insertion sort
 
-# high-level view of insertion sort:
-# sort array `array` as follows:
-# - create empty ordered array `sorted`
-# - insert elements from `array` into `sorted` one by one,
-# while preserving the "order" property of `sorted` at each step
-# - return `sorted`
+# high-level view of insertion sort in place:
+# - iterate over array indices starting with 1
+# - save the value in array[index]
+# - shift values at indices smaller than index 1 place to the right
+# - until you find value smaller than saved value
+# - insert saved value in gap
+
+# - loop invariant: "the array array[0...index]" is sorted
+
 
 # sort given array
 def sort(array)
-  sorted = []
-  for index in (0...array.size)
-    sorted = order_insert(array[index], sorted)
-  end
-  sorted
-end
-
-# insert value in ordered array, returning an order
-def order_insert(value, order)
-  return [value] if order == []
-  # ^ edge case; won't happen if we call from sort
-  index = 0
-  loop do
-    if value <= order[index]
-      order = insert_at(index, value, order)
-      break
+  for index in (1...array.size)
+    stored = array[index]
+    position = index
+    while position > 0 && array[position - 1] > stored
+      array[position] = array[position - 1]
+      position -= 1
     end
-    index += 1
-    if index == order.size
-      order[index] = value
-      break
-    end
-  end
-  order
-end
-
-# insert value in array at index
-def insert_at(index, value, array)
-  return nil if index > array.size
-  # ^ edge case; won't happen if we call from insert
-  breakpoint = array.size + 1
-  value_to_insert = value
-  loop do
-    value_stored = array[index]
-    array[index] = value_to_insert
-    value_to_insert = value_stored
-    index += 1
-    break if index == breakpoint
+    array[position] = stored
   end
   array
 end
 
-p sort([10, 5, 3, 0]) # [0, 3, 5, 10]
-p sort([]) # []
-p sort([1, 3, 10, 4]) # [1, 3, 4, 10]
-p sort([1, 1, 1, 1, 1]) # [1, 1, 1, 1, 1]
+p sort([4, 2, 3, 1])
+p sort([1, 3, 5, 4, 5])
+p sort([])
+p sort([1])
