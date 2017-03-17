e24f4d82b96acbc5cdf5adb950b1b754d6ef4d47
Ben Rodenhäuser <ben@karo.berlin>
2017-03-17 18:16:17 +0100

    selection sort

---
+++ my_exercises/sort/quicksort.rb
@@ -0,0 +1,4 @@
+# choose pivot
+# divide the arrays in two halves: smaller than pivot goes to the left, larger than pivot goes to the right
+# repeat
+# if you hit arrays of length 0 or 1, do nothing
