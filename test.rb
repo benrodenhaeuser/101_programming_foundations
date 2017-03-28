18d7cd59ba390ad8133ab4a94d088ff7c212d306
Ben Rodenhäuser <ben@karo.berlin>
2017-03-29 00:37:08 +0200

    cleanup

--- test.rb
+++ test.rb
@@ -1,22 +1,11 @@
-arr = ['10', '11', '9', '7', '8']
+# hash = { key: 'value' }
+# array = [1, 2, 3]
+#
+# p puts(hash, array).object_id == puts([hash, array]).object_id
 
-p arr.sort do |a, b|
-  b.to_i <=> a.to_i
-end
-# ["10", "11", "7", "8", "9"]
 
-p (arr.sort do |a, b|
-  b.to_i <=> a.to_i
-end)
-# ["11", "10", "9", "8", "7"]
+puts([[1, 2,], 3])
 
-p arr.sort do |a, b| end
-# ["10", "11", "7", "8", "9"]
+puts [1, 2], 3
 
-result = arr.sort do |a, b|
-  b.to_i <=> a.to_i
-end
-
-p result
-
-# ["11", "10", "9", "8", "7"]
+print([1, 2])
